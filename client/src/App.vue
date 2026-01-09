77b78ac09c22ce24323bbc90002a12f4978de81a
phatthanan <phatthananb66@nu.ac.th>
2026-01-09 08:30:58 -0800

    client

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style>
+ 
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #e6e6e7;
+  margin-top: 20px;
+}
+</style>
---
+++ client/src/components/Menus/ShowMenu.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Show Menu</h1>
+    <p>menu ID: {{ menuId }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      menuId: null
+    }
+  },
+  created () {
+    // โค้ดนี้ถูกรันทันทีที่ Component ถูกสร้างขึ้น (ก่อนเมาท์)
+    this.menuId = this.$route.params.menuId 
+    // ณ จุดนี้ this.userId ได้รับค่าแล้ว และจะถูกใช้ในการ Render UI
+  }
+}
+</script>
